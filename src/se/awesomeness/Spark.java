057e38c79c7a510a156d7cb83b280deb491f62af
Karl.a.Blixt <karl.a.blixt@gmail.com>
2021-09-13 14:05:24 +0200

    Core structure

--- src/se/awesomeness/Spark.java
+++ src/se/awesomeness/Spark.java
@@ -1,7 +1,33 @@
 package se.awesomeness;
 
 import robocode.Robot;
+import robocode.AdvancedRobot;
+public class Spark extends AdvancedRobot {
 
-public class Spark extends Robot {
+    public void run(){
+        back(100);
+        //noinspection InfiniteLoopStatement
+        while (true){
 
+            calculateMove();
+            calculateRadar();
+            calculateFire();
+            execute();
+        }
+
+    }
+    public void calculateMove(){
+        setAhead(1000);
+        setTurnLeft(40);
+    }
+
+    public void calculateRadar(){
+        setTurnRadarLeft(90);
+    }
+
+    public void calculateFire(){
+        setFire(3);
+        setTurnGunLeft(20);
+        setAhead(50);
+    }
 }
