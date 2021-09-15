5571b497e58f691334d7a088ff3b8bb1f79a54ef
HarisAvdicevic <haris.avdi93@gmail.com>
2021-09-15 15:18:34 +0200

    Add calculateRadar

--- src/se/awesomeness/Spark.java
+++ src/se/awesomeness/Spark.java
@@ -1,10 +1,29 @@
 package se.awesomeness;
 
 import robocode.Robot;
+import robocode.ScannedRobotEvent;
+
 public class Spark extends Robot {
 
-    public void run(){
+
+    public void run() {
+
+
+        while (true) {
+            calulateRadar();
 
         }
-}
+    }
+
+    public void calulateRadar() {
+
+            // Turns the radar 180 degrees to the right.
+            turnRadarRight(180);
+
+    }
+
+
+
+    }
+
 
