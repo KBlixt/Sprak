d8aaba45fccd788681cda014094f044552fa570a
Karl.a.Blixt <karl.a.blixt@gmail.com>
2021-09-18 10:49:13 +0200

    add Function

---
+++ src/se/awesomeness/Function.java
@@ -0,0 +1,30 @@
+package se.awesomeness;
+
+public class Function {
+    FunctionType type;
+    double value;
+    public Function(FunctionType type, double value){
+        this.type = type;
+        this.value = value;
+    }
+
+    public Point closestPoint(Point point){
+        Point closestPoint = new Point();
+
+
+        return closestPoint;
+    }
+
+    public Point functionIntersect(Function function){
+        Point intersection = new Point();
+
+
+        return intersection;
+    }
+
+    public boolean pointWithinFunction(){
+        boolean pointWithinFunction = false;
+
+        return pointWithinFunction;
+    }
+}
---
+++ src/se/awesomeness/FunctionType.java
@@ -0,0 +1,8 @@
+package se.awesomeness;
+
+public enum FunctionType {
+    Y_ACC_LIMIT,
+    X_ACC_LIMIT,
+    Y_WALL,
+    X_WALL
+}
