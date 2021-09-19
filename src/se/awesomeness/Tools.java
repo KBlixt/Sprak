a002b902df4f9dac16deefcdf2e10b9205dd79d6
Karl.a.Blixt <karl.a.blixt@gmail.com>
2021-09-19 23:14:43 +0200

    add Tools.round

--- src/se/awesomeness/Tools.java
+++ src/se/awesomeness/Tools.java
@@ -13,4 +13,8 @@ public class Tools {
     public static double convertAngle(double angle){
         return 90-angle;
     }
+
+    public static double round(double number){
+        return (double)Math.round(number*1_000_000)/1_000_000;
+    }
 }
