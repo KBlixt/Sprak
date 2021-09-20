f9ac892a9ae157ade22ab6a01873a9642582aa2b
Karl.a.Blixt <karl.a.blixt@gmail.com>
2021-09-20 11:46:11 +0200

    fix Tools.shortestAngle

--- src/se/awesomeness/Tools.java
+++ src/se/awesomeness/Tools.java
@@ -4,14 +4,17 @@ public class Tools {
 
     public static double shortestAngle(double angle){
         angle = angle % 360;
+
         if ( angle > 180) {
             angle -= 360;
+        }else if (angle < -180){
+            angle += 360;
         }
         return angle;
     }
 
     public static double convertAngle(double angle){
-        return 90-angle;
+        return shortestAngle(90-angle);
     }
 
     public static double round(double number){
