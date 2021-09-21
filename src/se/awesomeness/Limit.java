388c312ebc8d449cc3cb370213113cfd25880756
Karl.a.Blixt <karl.a.blixt@gmail.com>
2021-09-21 20:20:03 +0200

    reverted code.

--- src/se/awesomeness/Limit.java
+++ src/se/awesomeness/Limit.java
@@ -30,31 +30,43 @@ public record Limit(LimitType type, double limit, double speed) {
         double magnitude = pointVector.getMagnitude();
         double direction = pointVector.getDirection();
         double margin = 1.00000001;
-        if (magnitude < 0){
-            magnitude *= 1;
-            direction = Tools.oppositeAngle(direction);
-        }
 
         switch (type) {
             case ACCELERATION_LIMIT -> {
-                if (limit<0){
+                if (magnitude < 0) {
                     magnitude *= 1;
                     direction = Tools.oppositeAngle(direction);
                 }
-                double magnitudeLimit = Math.abs(speed+limit);
+                double magnitudeLimit = Math.abs(speed + limit);
                 boolean xIsPositiv = direction <= 90 && direction >= -90;
+                if (limit > 0) {
+                    boolean withinPositivX = magnitude <= magnitudeLimit * margin && xIsPositiv;
+                    boolean outsideNegativeX = magnitude >= magnitudeLimit / margin && !xIsPositiv;
+                    return withinPositivX || outsideNegativeX;
+                } else if (limit < 0) {
+                    boolean outsidePositivX = magnitude >= magnitudeLimit / margin && xIsPositiv;
+                    boolean insideNegativeX = magnitude <= magnitudeLimit * margin && !xIsPositiv;
+                    return outsidePositivX || insideNegativeX;
+                } else {
+                    return Math.abs(magnitude) / margin < Math.abs(speed);
+                }
 
-                boolean withinPositivX = magnitude <= magnitudeLimit*margin && xIsPositiv;
-                boolean outsideNegativeX = magnitude >= magnitudeLimit/margin && !xIsPositiv;
-                return withinPositivX || outsideNegativeX;
+
             }
             case TURN_LIMIT -> {
-                if (limit<0){
+                if (magnitude < 0) {
                     direction = Tools.oppositeAngle(direction);
                 }
-                boolean underPositiveX = direction <= limit*margin && direction >= -90*margin;
-                boolean overNegativeX = direction >= 90/margin || direction <= Tools.oppositeAngle(limit)/margin;
-                return underPositiveX || overNegativeX;
+                if (limit > 0) {
+                    boolean underPositiveX = direction <= limit * margin && direction >= -90 * margin;
+                    boolean overNegativeX = direction >= 90 / margin || direction <= Tools.oppositeAngle(limit) / margin;
+                    return underPositiveX || overNegativeX;
+                } else {
+                    boolean overPositiveX = direction >= limit * margin && direction <= 90 * margin;
+                    boolean underNegativeX = direction <= -90 / margin || direction >= Tools.oppositeAngle(limit) / margin;
+                    return overPositiveX || underNegativeX;
+                }
+
             }
             default -> {
                 System.out.println("<ERROR> no type match in switch in Function.withinFunction. type: " + type.name());
