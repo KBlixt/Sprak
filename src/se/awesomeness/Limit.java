701e694e29a2f8a19d31b3490da57632933a5bd0
Karl.a.Blixt <karl.a.blixt@gmail.com>
2021-09-24 00:17:19 +0200

    fixed Limit.withinLimit

--- src/se/awesomeness/Limit.java
+++ src/se/awesomeness/Limit.java
@@ -36,12 +36,12 @@ public record Limit(LimitType type, double limit) {
             magnitude *= -1;
             direction = Tools.oppositeAngle(direction);
         }
+        boolean xIsPositiv = direction <= 90 && direction >= -90;
 
         double margin = 0.000000001;
         boolean withinLimit = false;
         switch (type) {
             case ACCELERATE_LIMIT -> {
-                boolean xIsPositiv = direction <= 90 && direction >= -90;
                 if (limit >= 0 ^ xIsPositiv){
                     withinLimit = limit >= 0;
                 }else{
@@ -51,7 +51,6 @@ public record Limit(LimitType type, double limit) {
                 }
             }
             case DECELERATE_LIMIT -> {
-                boolean xIsPositiv = direction <= 90 && direction >= -90;
                 if (limit >= 0 ^ xIsPositiv){
                     withinLimit = limit <= 0;
                 }else{
@@ -60,12 +59,20 @@ public record Limit(LimitType type, double limit) {
                     withinLimit = withinLimitPosX || withinLimitNegX;
                 }
             }
-            case TURN_LEFT_LIMIT ->
-                    withinLimit = direction <= limit + margin || direction >= Tools.oppositeAngle(limit) - margin;
-
-            case TURN_RIGHT_LIMIT ->
-                    withinLimit = direction >= limit - margin || direction <= Tools.oppositeAngle(limit) + margin;
-
+            case TURN_LEFT_LIMIT ->{
+                if (xIsPositiv){
+                    withinLimit = direction <= limit + margin;
+                }else{
+                    withinLimit = direction <= Tools.oppositeAngle(limit) + margin || direction >= 90 - margin;
+                }
+            }
+            case TURN_RIGHT_LIMIT -> {
+                if (xIsPositiv) {
+                    withinLimit = direction >= limit - margin;
+                } else {
+                    withinLimit = direction >= Tools.oppositeAngle(limit) - margin || direction <= -90 + margin;
+                }
+            }
             default ->
                     System.out.println("<ERROR> no type match in switch in Function.withinFunction. type: " + type.name());
         }
