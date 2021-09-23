61fa78c331f355354da3b964b39db69102e7ef97
Karl.a.Blixt <karl.a.blixt@gmail.com>
2021-09-24 00:27:15 +0200

    reworked and combined Mover.MaxForceVector into Mover.move
    comment: :)

--- src/se/awesomeness/Mover.java
+++ src/se/awesomeness/Mover.java
@@ -26,24 +26,26 @@ public class Mover {
         System.out.println("speed: " + speed);
         System.out.println("heading: " + heading);
         forces.add(sprak.normalVelocity);
-        forces.add(new Vector(9,45));
+        //forces.add(new Point(sprak.getBattleFieldWidth()/2,sprak.getBattleFieldHeight()/2).vectorTo(sprak.position));
 
 
         Vector forceSum = Vector.addAll(forces);
         System.out.println("forceSum: " + forceSum);
 
-        expandForce(forceSum);
+        double lookAhead = 70;
+        double wallBuffer = 20;
+
+        expandForce(forceSum, lookAhead);
         System.out.println("forceSum after exp: " + forceSum);
 
-        Vector forceAfterWall = wallSurfing(forceSum);
+        Vector forceAfterWall = wallSurfing(forceSum, wallBuffer);
         System.out.println("forceAfterWall: " + forceAfterWall);
         move(forceAfterWall);
     }
 
     
-    public Vector wallSurfing(Vector force){
+    public Vector wallSurfing(Vector force, double wallOffset){
         double magnitude = force.getMagnitude();
-        double wallOffset = 2;
         Point position = new Point(
                 sprak.position.getX(),
                 sprak.position.getY());
@@ -72,6 +74,10 @@ public class Mover {
         List<Point> pointsOnWall = new ArrayList<>();
 
         pointsOnWall.add(force.getFreeForm());
+        pointsOnWall.add(new Point(new Vector(magnitude, 0)));
+        pointsOnWall.add(new Point(new Vector(magnitude, 90)));
+        pointsOnWall.add(new Point(new Vector(magnitude, 180)));
+        pointsOnWall.add(new Point(new Vector(magnitude, -90)));
         if (Math.abs(distToWallX) < magnitude) {
             pointsOnWall.add(new Point(distToWallX, magnitude*Math.sqrt(1-(distToWallX/magnitude))));
             pointsOnWall.add(new Point(distToWallX, -magnitude*Math.sqrt(1-(distToWallX/magnitude))));
@@ -89,8 +95,24 @@ public class Mover {
         boolean withinLimits = false;
         Point candidatePoint = new Point();
         while (!withinLimits) {
-            candidatePoint = force.getFreeForm().closestPoint(pointsOnWall);
-            pointsOnWall.remove(candidatePoint);
+            if (!pointsOnWall.isEmpty()) {
+                candidatePoint = force.getFreeForm().closestPoint(pointsOnWall);
+                pointsOnWall.remove(candidatePoint);
+            }else{
+                List<Point> edges = List.of(
+                        new Point(new Vector(magnitude, 0)),
+                        new Point(new Vector(magnitude, 90)),
+                        new Point(new Vector(magnitude, 180)),
+                        new Point(new Vector(magnitude, -90)));
+                List<Point> edgesOnMap = new ArrayList<>();
+                for (Point edge : edges) {
+                    edgesOnMap.add(sprak.position.addVector(new Vector(edge)));
+                }
+                Point candidatePointOnMap = new Point(maxX/2, maxY/2).closestPoint(edgesOnMap);
+                candidatePoint = candidatePointOnMap.subtractVector(new Vector(sprak.position));
+
+                break;
+            }
             Point checker = sprak.position.addVector(new Vector(candidatePoint));
             double margin = 1;
             boolean insideX = checker.getX()  - margin<= maxX && checker.getX() + margin>= minX;
@@ -103,21 +125,76 @@ public class Mover {
         return force;
     }
 
-    public void expandForce(Vector force){
+    public void expandForce(Vector force, double targetMagnitude){
         if (force.getMagnitude() < 0){
             force = force.negative();
         }
-        force.setVector(150, force.getDirection());
+        force.setVector(targetMagnitude, force.getDirection()); //118 safe in 90degree turns at speed 8.
     }
 
 
-    public void move(Vector forceVector){
+    public void move(Vector force){
         double speed = sprak.normalVelocity.getMagnitude();
         double heading = sprak.normalVelocity.getDirection();
+        if (force.getMagnitude() < 0){
+            force = force.negative();
+        }
+        force = new Vector(force.getMagnitude(), force.getDirection() - heading);
 
-        forceVector = MaxForceVector(forceVector);
-        Vector moveVector = toMoveVector(forceVector, speed , heading );
-        System.out.println("MoveVector: " + moveVector);
+        double upperSpeedLimit = speed + Math.min(2, Math.max(-speed, Math.min(1, -speed + 8)));
+        double lowerSpeedLimit = speed + Math.max(-8 - speed, Math.min(-1, Math.max(-2, -speed)));
+        double leftTurnLimit = 10 - 0.75 * Math.abs(speed);
+        double rightTurnLimit = -leftTurnLimit;
+
+        List<Limit> limits = List.of(
+                new Limit(LimitType.ACCELERATE_LIMIT, upperSpeedLimit),
+                new Limit(LimitType.DECELERATE_LIMIT, lowerSpeedLimit),
+                new Limit(LimitType.TURN_LEFT_LIMIT, leftTurnLimit),
+                new Limit(LimitType.TURN_RIGHT_LIMIT, rightTurnLimit)
+        );
+
+        List<Point> candidatePoints = new ArrayList<>();
+        candidatePoints.add(new Point(new Vector(upperSpeedLimit, leftTurnLimit)));
+        candidatePoints.add(new Point(new Vector(upperSpeedLimit, rightTurnLimit)));
+        candidatePoints.add(new Point(new Vector(lowerSpeedLimit, leftTurnLimit)));
+        candidatePoints.add(new Point(new Vector(lowerSpeedLimit, rightTurnLimit)));
+
+        Vector zeroOffset = new Vector((upperSpeedLimit + lowerSpeedLimit)/2, (leftTurnLimit + rightTurnLimit)/2);
+        double circleProjectionRadius = zeroOffset.getFreeForm().furthestPoint(candidatePoints).distanceToPoint(zeroOffset.getFreeForm());
+        double largestTurnLimit = Math.max(Math.abs(leftTurnLimit), Math.abs(rightTurnLimit));
+        double distanceToUpperLineProjection = Math.cos(Math.toRadians(largestTurnLimit))*upperSpeedLimit;
+        double distanceToLowerLineProjection = Math.cos(Math.toRadians(largestTurnLimit))*lowerSpeedLimit;
+
+        double forceLineProjectionDistance;
+        if (force.getDirection()< 90 && force.getDirection() > -90){
+            forceLineProjectionDistance = distanceToUpperLineProjection/Math.cos(Math.toRadians(force.getDirection()));
+        }else if (force.getDirection()> 90 || force.getDirection() < -90){
+            forceLineProjectionDistance = distanceToLowerLineProjection/Math.cos(Math.toRadians(force.getDirection()));
+        }else{
+            forceLineProjectionDistance = Double.MAX_VALUE;
+        }
+        Vector forceLineProjection = new Vector(forceLineProjectionDistance, force.getDirection());
+        Vector zeroToForceLineProjection = forceLineProjection.subtract(zeroOffset);
+        Vector forceCircleProjection = new Vector(circleProjectionRadius, zeroToForceLineProjection.getDirection());
+        Point targetPoint = forceCircleProjection.add(zeroOffset).getFreeForm();
+
+        for (Limit limit : limits) {
+            candidatePoints.add(limit.closestPoint(targetPoint));
+        }
+
+        boolean withinLimits = false;
+        Point candidatePoint = new Point();
+
+        while (!withinLimits) {
+            withinLimits = true;
+            candidatePoint = targetPoint.closestPoint(candidatePoints);
+            for (Limit limit : limits) {
+                withinLimits &= limit.withinLimit(candidatePoint);
+            }
+            candidatePoints.remove(candidatePoint);
+        }
+        Vector moveVector = new Vector(candidatePoint);
+        System.out.println("moveVector: " + moveVector);
 
         if (moveVector.getDirection() < -90 || moveVector.getDirection() > 90){
             sprak.setVelocityRate(-moveVector.getMagnitude());
@@ -128,55 +205,8 @@ public class Mover {
         }
     }
 
-    public Vector MaxForceVector(Vector forceVector){
-        double speed = sprak.normalVelocity.getMagnitude();
-        double heading = sprak.normalVelocity.getDirection();
-        Point targetPoint = new Point(toMoveVector(forceVector, speed, heading));
+    public static Vector toMoveVector(Vector forceVector, double heading){
+        return new Vector(forceVector.getMagnitude(), forceVector.getDirection() - heading);
 
-        double upperAccLimit = Math.min(2, Math.max(-speed,Math.min(1, -speed+8)));
-        double lowerAccLimit = Math.max(-8-speed,Math.min(-1,Math.max(-2, -speed)));
-        double leftTurnLimit = 10 - 0.75 * Math.abs(speed);
-        double rightTurnLimit = -leftTurnLimit;
-
-        List<Limit> limits = List.of(
-                new Limit(LimitType.ACCELERATE_LIMIT, upperAccLimit + speed),
-                new Limit(LimitType.DECELERATE_LIMIT, lowerAccLimit + speed),
-                new Limit(LimitType.TURN_LEFT_LIMIT, leftTurnLimit),
-                new Limit(LimitType.TURN_RIGHT_LIMIT, rightTurnLimit)
-        );
-
-        List<Point> candidatePoints = new ArrayList<>();
-        for (Limit limit : limits) {
-            candidatePoints.add(limit.closestPoint(targetPoint));
-        }
-        candidatePoints.add(new Point(new Vector(speed +upperAccLimit,leftTurnLimit)));
-        candidatePoints.add(new Point(new Vector(speed +lowerAccLimit,leftTurnLimit)));
-        candidatePoints.add(new Point(new Vector(speed +upperAccLimit,rightTurnLimit)));
-        candidatePoints.add(new Point(new Vector(speed +lowerAccLimit,rightTurnLimit)));
-
-        boolean withinLimits = false;
-        Point candidatePoint = new Point();
-        while(!withinLimits){
-            withinLimits = true;
-            candidatePoint = targetPoint.closestPoint(candidatePoints);
-            for (Limit limit : limits) {
-                withinLimits &= limit.withinLimit(candidatePoint);
-            }
-            candidatePoints.remove(candidatePoint);
-        }
-
-        return toForceVector(new Vector(candidatePoint), speed, heading);
-    }
-
-    public static Vector toMoveVector(Vector forceVector, double speed, double heading){
-        Vector moveVector = new Vector(forceVector.getMagnitude(), forceVector.getDirection() - heading);
-        moveVector = moveVector.add(new Vector(speed,0));
-        return moveVector;
-    }
-
-    public static Vector toForceVector(Vector moveVector, double speed, double heading){
-        Vector vector = moveVector.subtract(new Vector(speed,0));
-        vector = new Vector(vector.getMagnitude(), vector.getDirection() + heading);
-        return vector;
     }
 }
